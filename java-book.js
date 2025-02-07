640bd56cc982da419e29007f9d22d60ff24e3963
Geoffrey Waite <186988887+waitegeoff2@users.noreply.github.com>
2025-02-07 13:22:08 -0500

    working on addtolibrary function to parse array

--- java-book.js
+++ java-book.js
@@ -1,6 +1,9 @@
+const bookLibrary = document.querySelector(".book-list");
+
 const myLibrary = [];
 
 function Book(title, author, pages, year) {
+    // this is the constructor
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -9,20 +12,31 @@ function Book(title, author, pages, year) {
     this.info = function() {
         return [this.title + " by " + this.author + ". Book has " + this.pages + " pages. Book was published in " + this.year + "."]
     };
-
-    // this is the constructor
 }
 
 function addBookToLibrary(title, author, pages, year) {
-    // create a book from arguments 
+    // take params, create a book THEN STORE IT IN ARRAY
     let newBook = new Book(title, author, pages, year);
     return myLibrary.push(newBook);
 
-    // take params, create a book THEN STORE IT IN ARRAY
+    //CALL THE DISPLAY LIBRARY FUNCTION to put on HTML page
 }
 
-// Object.setPrototypeOf(addBookToLibrary.prototype, Book.prototype);
+function displayLibrary() {
+    function addBook(arrayItem) {
+        
+        // take each array item and add it as a div to the table created earlier
+    };
+    
+    myLibrary.forEach(addBook);
+    // loop the mylibrary array for each item
+    // break up array and separate into items
+    // display on HTML table, APPEND EACH CHILD TO a table row 
+            // (https://stackoverflow.com/questions/63356133/loop-through-an-array-of-objects-and-display-in-html)
+}
 
+
+//MANUALLY ADDED BOOKS
 const catcherInTheRye = new Book("Catcher in the Rye", "JD Salinger", "240", "1951");
 
 console.log(addBookToLibrary("Catcher in the Rye", "JD Salinger", "240", "1951"));
@@ -30,23 +44,3 @@ console.log(addBookToLibrary("Catcher in the Rye", "JD Salinger", "240", "1951")
 console.log(myLibrary);
 
 
-// REFERENCE FROM LAST LESSON
-
-// function book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     this.read = read;
-//     // this.info = function() {
-//     //     console.log(this.title,"by", this.author,",", this.pages, ",", this.read);
-//     // }
-
-    this.info = function() {
-        return [this.title + " by " + this.author + ", " + this.pages + ", " + this.read]
-    };
-
-// };
-
-// const theHobbit = new book("The Hobbit", "JRR Tolkein", "295 pages", "not read yet");
-
-// console.log(theHobbit.info());
